685a45e3859b8429499d010363f65733efe91282
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2026-02-12 16:20:02 +0000

    fixed most compile errors, removed tinyxml2, event_logger from runner dependencies and updated the api to match new event subsystem

--- src/perception_runners.cpp
+++ src/perception_runners.cpp
@@ -1,9 +1,9 @@
 #include <pluginlib/class_list_macros.hpp>
 #include <capabilities2_runner/runner_base.hpp>
-#include <nav2_capabilities/waypoint_runner.hpp>
-#include <nav2_capabilities/occupancygrid_runner.hpp>
-#include <nav2_capabilities/robotpose_runner.hpp>
+// #include <nav2_capabilities/waypoint_runner.hpp>
+// #include <nav2_capabilities/occupancygrid_runner.hpp>
+// #include <nav2_capabilities/robotpose_runner.hpp>
 
-// register runner plugins
-PLUGINLIB_EXPORT_CLASS(capabilities2_runner::WayPointRunner, capabilities2_runner::RunnerBase)
-PLUGINLIB_EXPORT_CLASS(capabilities2_runner::RobotPoseRunner, capabilities2_runner::RunnerBase)
+// // register runner plugins
+// PLUGINLIB_EXPORT_CLASS(capabilities2_runner::WayPointRunner, capabilities2_runner::RunnerBase)
+// PLUGINLIB_EXPORT_CLASS(capabilities2_runner::RobotPoseRunner, capabilities2_runner::RunnerBase)
